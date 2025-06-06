d16f8aab51c99206affa80701b8e3cb1c7da2dc2
Eduardo-J-Morales <eduardojosegonzalezmorales41@gmail.com>
2025-06-06 09:03:13 -0400

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using UglyToad.PdfPig;
+using Xceed.Words.NET;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
+public class FileData {
+    public int Id { get; set; }
+    public string FileName { get; set; } = "";
+    public long Length { get; set; }
+    public string ExtractedText { get; set; } = "";
+}
+
+public class AppDbContext : DbContext {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {  }
+}
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
 
 app.Run();
