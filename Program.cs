580dea91b219d2edb791031f5cef78beb4ae5af4
Eduardo-J-Morales <eduardojosegonzalezmorales41@gmail.com>
2025-06-06 09:14:26 -0400

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -6,16 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 
-public class FileData {
-    public int Id { get; set; }
-    public string FileName { get; set; } = "";
-    public long Length { get; set; }
-    public string ExtractedText { get; set; } = "";
-}
-
-public class AppDbContext : DbContext {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {  }
-}
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql("Host=localhost;Database=yourdb;Username=youruser;Password=yourpassword"));
 
 var app = builder.Build();
 
@@ -23,5 +15,29 @@ app.UseHttpsRedirection();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+app.MapPost("/api/upload", async (HttpRequest request, AppDbContext db) =>
+{
+    var form = await request.ReadFormAsync();
+    var files = form.Files;
+    var results = new List<object>();
+
+    foreach (var file in files)
+    {
+        string text = "";
+    }
+});
 
 app.Run();
+
+public class FileData
+{
+    public int Id { get; set; }
+    public string FileName { get; set; } = "";
+    public long Length { get; set; }
+    public string ExtractedText { get; set; } = "";
+}
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<FileData> Files { get; set; }
+}
